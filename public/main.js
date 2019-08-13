070636ce9484297d22015f9487b77db3d55736b4
hayesch19 <chrishayespro@gmail.com>
2019-08-13 17:29:41 -0400

    V1 HTML/CSS/Search Function Added

--- public/main.js
+++ public/main.js
@@ -1,7 +1,29 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+const city = document.querySelector('.location-input')
+
+// Create Search
+const createSearch = () => {
+  const inputValue = city.value
+  const url = 'https://api.openweathermap.org/data/2.5/weather'
+  const apiKey = '327b0be96e656ea859661f0ccae3fab5'
+  const newUrl =
+    url + '?' + 'q=' + inputValue + '&units=imperial' + '&appid=' + apiKey
+  return newUrl
+}
+// Search Results
+const searchResults = () => {
+  // eslint-disable-next-line no-undef
+  fetch(createSearch())
+    .then(response => {
+      return response.json()
+    })
+    .then(city => {
+      console.log(city)
+      document.querySelector('.city-name').textContent = city.name
+      document.querySelector('.weather').textContent = city.weather[0].main
+      document.querySelector('.temp').textContent = city.main.temp
+    })
 }
 
-document.addEventListener('DOMContentLoaded', main)
+document
+  .querySelector('.get-weather-btn')
+  .addEventListener('click', searchResults)
